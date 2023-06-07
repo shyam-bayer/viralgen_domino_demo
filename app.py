ca6ef5024a14c92db3cd973b6cb4bf1ad756bb3c
shyam-bayer <96440494+shyam-bayer@users.noreply.github.com>
2023-06-06 22:55:13 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,105 +1,73 @@
-'''
-
-Boilerplate script for new Dash applications.
-
-Original author: John Mason
-Date: May 2021
-
-This script is meant to serve as a starting place for developing a Dash 
-application.  Features:
-- Fixes Dash URLs for launching in a Domino workspace or deploying on Domino.
-- Imports for the basic (and some advanced) components libraries.
-- Imports and setup for on-disk caching of intermediates.  This code is disabled
-    by default.
-
-I recommend running any application via a Flask debugger configuration in VS 
-Code.  This script should be distributed with a proper launch configuration.
-
-'''
-
-import sys
-import os
-import argparse
-
 import dash
-import dash.dependencies as dd # for .Input, .Output, .State
-import dash_core_components as dcc # Basic input/output, figures
-import dash_html_components as html # Standard HTML tags
-import dash_bootstrap_components as dbc # Layout, advanced widgets and styling
-# import dash_table # Advanced table component (.DataTable)
-# See Dash documentation for other component libraries
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
+import os
 
-import plotly.express as px # High-level plotting
-import plotly.graph_objects as go # Low-level plotting
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/plotly/'
+    'datasets/master/gapminderDataFiveYear.csv')
 
-# from flask_caching import Cache # For on-disk caching
+app = dash.Dash()
 
-try:
-    # These arguments are added when launching the Flask debugger, but for some
-    # reason aren't removed from the arguments before launching the Python 
-    # application.
-    sys.argv.remove('run')
-    sys.argv.remove('--no-debugger')
+# Configure Dash to recognize the URL of the container
+user = os.environ.get("DOMINO_PROJECT_OWNER")
+project = os.environ.get("DOMINO_PROJECT_NAME")
+runid = os.environ.get("DOMINO_RUN_ID")
+runurl = '/' + user + '/' + project + '/r/notebookSession/'+ runid + '/'
 
-except ValueError:
-    pass
+app.config.update({
+'routes_pathname_prefix': runurl,
+'requests_pathname_prefix': runurl
+})
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--deploy', action = 'store_true')
+# Set layout
+app.layout = html.Div(style={'paddingLeft': '40px', 'paddingRight': '40px'}, children=[
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        id='year-slider',
+        min=df['year'].min(),
+        max=df['year'].max(),
+        value=df['year'].min(),
+        step=None,
+        marks={str(year): str(year) for year in df['year'].unique()}
+    )
+])
 
-args = parser.parse_args()
 
-if args.deploy:
-    # See https://docs.dominodatalab.com/en/4.2/reference/publish/apps/Getting_started_with_Dash.html
-    HOST = '0.0.0.0'
-    PORT = '8888'
+@app.callback(
+    dash.dependencies.Output('graph-with-slider', 'figure'),
+    [dash.dependencies.Input('year-slider', 'value')])
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+    traces = []
+    for i in filtered_df.continent.unique():
+        df_by_continent = filtered_df[filtered_df['continent'] == i]
+        traces.append(go.Scatter(
+            x=df_by_continent['gdpPercap'],
+            y=df_by_continent['lifeExp'],
+            text=df_by_continent['country'],
+            mode='markers',
+            opacity=0.7,
+            marker={
+                'size': 15,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name=i
+        ))
 
-    REQUESTS_PREFIX = '/'.join([
-        '',
-        '{DOMINO_PROJECT_OWNER}',
-        '{DOMINO_PROJECT_NAME}',
-        'r',
-        'notebookSession',
-        '{DOMINO_RUN_ID}',
-        ''
-    ]).format(**os.environ)
+    return {
+        'data': traces,
+        'layout': go.Layout(
+            xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest'
+        )
+    }
 
-else:
-    HOST = '127.0.0.1' # Default for Dash applications
-    PORT = '5000' # Default for Flask debugging
-
-    REQUESTS_PREFIX = '/'.join([
-        '',
-        '{DOMINO_STARTING_USERNAME}', # might need to be DOMINO_PROJECT_OWNER
-        '{DOMINO_PROJECT_NAME}',
-        'notebookSession',
-        '{DOMINO_RUN_ID}',
-        'proxy',
-        str(PORT),
-        ''
-    ]).format(**os.environ)
-    print(f'Open https://domino.sats.cloud{REQUESTS_PREFIX} in your browser')
-
-app = dash.Dash(
-    __name__,
-    external_stylesheets = [dbc.themes.BOOTSTRAP],
-    requests_pathname_prefix = REQUESTS_PREFIX
-)
-application = app.server
-
-# cache = Cache( # Bind to function with @cache.memoize(timeout = <seconds>)
-#     application,
-#     config = {
-#         'CACHE_TYPE': 'FileSystemCache',
-#         'CACHE_DIR': 'flask-cache'
-#     }
-# )
-
-# Application code goes here
-
-app.layout = dbc.Container(
-    'Hello world'
-)
 
 if __name__ == '__main__':
-    app.run_server(debug = False, port = PORT, host = HOST)
+    app.run_server(port=8888, host='0.0.0.0', debug=True)
